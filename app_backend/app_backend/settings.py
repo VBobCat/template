1dc488621465fd3f7f77199f677faac2e6fc7e2e
Antonio Carlos Jorge Patricio <antonio.patricio@agu.gov.br>
2021-09-18 10:20:09 -0300

    angular

--- app_backend/app_backend/settings.py
+++ app_backend/app_backend/settings.py
@@ -79,6 +79,6 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [BASE_DIR / 'angular-frontend/dist/angular-frontend']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
